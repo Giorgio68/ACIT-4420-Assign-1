1c0474a58dac7f4ed44edaa815fc541163dfd187
Giorgio Salvemini <giorgio@salvemini.no>
2024-09-16 12:25:38 +0200

    Completed task B

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ACIT4420 Assignment 1
+
+## Task A
+
+## Task B
+
+Seeing as most bank accounts have similar functions, allowing the user to withdraw and deposit cash, it was worth using inheritance to avoid rewriting the same code multiple times. A base `BankAccount` class was written with a constructor, a method to deposit money, one to withdraw it, and finally a way to get account information, in the form of a string containing the account holder and balance.
+
+As a checking account has a fee for withdrawals, the base `withdraw` method is overridden, to add the fee to the requested amount; this value is then passed to the base `withdraw` method by calling `super()`.
+
+The savings account remains the same as the base class, except for a new method to apply interest. This will simply multiply the current balance by 1.025, effectively adding an interest of 2.5% on the balance.
+
+One change that was done compared to the original `BankAccount` class was to return the available balance from the `deposit` and `withdraw` methods, allowing to test the code programmatically and using assertions, rather than having to look at a console output; a new method was also added to return the available balance, for the same reason.
+
+Additionally, both the transaction fee for the checking account and the interest rate for the savings account were created as class members rather than instance members; the reason for this choice is that generally these tend to be the same for all accounts, which means that it can make sense that it should be the same object for all instances.
+
+A series of test cases were written to demonstrate and test the functionality of the three classes, such as testing that depositing and withdrawing money works as expected, and for checking and savings accounts respectively, that the transaction fee is also subtracted from the balance and that the interest rate is applied correctly. Assertions were used during tests, easily allowing a tester to see whether a test has failed or all have succeeded.
---
+++ task_b.py
@@ -0,0 +1,215 @@
+"""
+ACIT 4420, Assignment 1, Task B
+
+Giorgio Salvemini
+"""
+
+
+class BankAccount:
+    """
+    Base class for creating bank accounts. Initializes balance to 0
+
+    :param account_holder: The name of the account owner
+    """
+    def __init__(self, account_holder: str) -> None:
+        """
+        Constructor method
+        """
+        self.account_holder = account_holder
+        self.balance = 0
+
+
+    def deposit(self, amount: float) -> float:
+        """
+        Adds an amount to the user's balance
+
+        :param amount: The amount to be added
+        :return: The updated balance
+        """
+        self.balance += amount
+
+        return self.balance
+
+
+    def withdraw(self, amount: float) -> float:
+        """
+        Subtracts an amount from the user's balance, or prints an error if the requested amount is
+        higher than the available balance
+
+        :param amount: The amount to withdraw
+        :return: The updated balance, or -1 if the balance is insufficient
+        """
+        if amount <= self.balance:
+            self.balance -= amount
+            return self.balance
+
+        print("Insufficient funds")
+        return -1
+
+
+    def balance_info(self) -> float:
+        """
+        Return the current balance
+
+        :return: The current balance
+        """
+        return self.balance
+
+
+    def account_info(self) -> str:
+        """
+        Returns a string containing the account holder's name and balance
+        """
+        return f"Account holder: {self.account_holder}, Balance: ${self.balance:.2f}"
+
+
+class CheckingAccount(BankAccount):
+    """
+    Class for checking accounts, inheriting from a base bank account. Adds a transaction fee to
+    all withdrawals
+
+    :param account_holder: The name of the account owner
+    """
+    transaction_fee: float = 2.5 # usd
+
+
+    def withdraw(self, amount: float) -> float:
+        """
+        Overrides the base `withdraw` method. Adds a transaction fee to the requested amount
+
+        :return: The updated balance
+        """
+        return super().withdraw(amount+self.transaction_fee)
+
+
+class SavingsAccount(BankAccount):
+    """
+    Class for savings accounts, inheriting from a base bank account. Allows to apply interest to
+    the available balance
+
+    :param account_holder: The name of the account owner
+    """
+    interest_rate: float = 0.025 # 2.5%
+
+
+    def apply_interest(self) -> float:
+        """
+        Applies the interest rate to the chosen
+
+        :return: The updated balance
+        """
+        self.balance *= 1 + self.interest_rate
+
+        return self.balance
+
+
+def test_base() -> None:
+    print(
+        """
+*************************
+Testing base bank account
+*************************
+"""
+    )
+    bank_account = BankAccount("Giorgio")
+    print(bank_account.account_info())
+
+    # basic functionality test
+    assert bank_account.deposit(1000) == 1000
+    print(bank_account.account_info())
+
+    assert bank_account.withdraw(500) == 500
+    print(bank_account.account_info())
+
+    assert bank_account.withdraw(500) == 0
+    print(bank_account.account_info())
+
+    assert bank_account.withdraw(500) == -1 # this should error out
+
+    try:
+        bank_account.deposit("a")
+    except TypeError as e:
+        print(repr(e))
+        print("Cannot deposit a string to the balance")
+
+    try:
+        bank_account.withdraw("a")
+    except TypeError as e:
+        print(repr(e))
+        print("Cannot withdraw a string from the balance")
+
+    print(
+        """
+****************************
+Base bank account tests done
+****************************
+"""
+    )
+
+
+def test_checking() -> None:
+    print(
+        """
+************************
+Testing checking account
+************************
+"""
+    )
+    checking_account = CheckingAccount("Giorgio")
+
+    assert checking_account.deposit(1000) == 1000
+    print(checking_account.account_info())
+
+    assert checking_account.withdraw(500) == 500-2.5 # subtract the transaction fee
+    print(checking_account.account_info())
+
+    assert checking_account.withdraw(500) == -1 # this should error out since there's less than 500
+
+    try:
+        checking_account.withdraw("a")
+    except TypeError as e:
+        print(repr(e))
+        print("Cannot withdraw a string from the balance")
+
+    print(
+        """
+********************************
+Base checking account tests done
+********************************
+"""
+    )
+
+
+def test_savings() -> None:
+    print(
+        """
+***********************
+Testing savings account
+***********************
+"""
+    )
+    savings_account = SavingsAccount("Giorgio")
+
+    assert savings_account.deposit(1000) == 1000
+    print(savings_account.account_info())
+
+    assert savings_account.apply_interest() == 1025 # 1000*(1 + 0.025)
+    print(savings_account.account_info())
+
+    print(
+        """
+*******************************
+Base savings account tests done
+*******************************
+"""
+    )
+
+
+def main() -> None:
+    test_base()
+    test_checking()
+    test_savings()
+
+
+if __name__ == "__main__":
+    main()
